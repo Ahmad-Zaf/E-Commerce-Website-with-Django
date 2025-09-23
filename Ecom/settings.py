7fe113ce59950972103b4abaa20ca2475d7eac92
Ahmad Zafar <az8414577@gmail.com>
2025-09-23 14:34:28 +0000

    added CSRF thing

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-p3+5&v!45)hzmi=ah*r8=*lh%qq=g_1dj02jwzga=%4s)cr1z%
 DEBUG = True
 
 ALLOWED_HOSTS = ['e-commerce-site-production-3893.up.railway.app', 'https://e-commerce-site-production-3893.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['e-commerce-site-production-3893.up.railway.app', 'https://e-commerce-site-production-3893.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://e-commerce-site-production-3893.up.railway.app']
 
 
 # Application definition
