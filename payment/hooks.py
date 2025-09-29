11c1b68f0a2e7b690e80cc80dc8c33533de6e988
Ahmad Zafar <az8414577@gmail.com>
2025-09-29 20:46:56 +0530

    Update hooks.py

--- payment/hooks.py
+++ payment/hooks.py
@@ -1,5 +1,5 @@
 from paypal.standard.models import ST_PP_COMPLETED
-from paypal.standart.ipn.signals import valid_ipn_received
+from paypal.standard.ipn.signals import valid_ipn_received
 from django.dispatch import receiver
 from django.conf import settings
 
